590850e4f8c4b50b98a62d9102b2be05b72f8f8a
Sinha, Sudhanshu <sudhanshu.sinha@optum.com>
2024-05-13 18:41:13 +0530

    5th commit

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,11 @@ terraform {
   #required_version = ">= 1.0.11"
   required_providers {
     google = {
-  #    version = ">= 4.25.0"
+      version = ">= 4.25.0"
       source  = "hashicorp/google"
     }
   }
+
   backend "remote" {
   # The name of your Terraform Cloud organization.
     organization = "terraform_cloud_org00"
